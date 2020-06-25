9a48cc8a6c74984caf0e21808adcbd2b9b9dedb0
Akshay Hadke <v-ahadke@expedia.com>
2020-06-25 16:47:10 +0530

    Push only latest tag instead of all tags

--- mirror_repo.sh
+++ mirror_repo.sh
@@ -15,4 +15,5 @@ git commit -a -m "patch cart file"
 git push "git@github.com:v-ahadke/public_repo.git" :refs/tags/$tag
 git tag -fa -m "$tag" $tag
 git push -f --all "git@github.com:v-ahadke/public_repo.git"
-git push -f --tags "git@github.com:v-ahadke/public_repo.git"
+#git push -f --tags "git@github.com:v-ahadke/public_repo.git"
+git push -f "git@github.com:v-ahadke/public_ios_repo.git" $tag

86bbf715f15bb0f6cf3e816397191aaaaba12b60
Akshay Hadke <v-ahadke@expedia.com>
2020-06-26 21:38:19 +0530

    Pass the same message to public github tag

--- mirror_repo.sh
+++ mirror_repo.sh
@@ -13,7 +13,10 @@ tag="$(git describe --tags)"
 git commit -a -m "patch cart file"
 
 git push "git@github.com:v-ahadke/public_repo.git" :refs/tags/$tag
-git tag -fa -m "$tag" $tag
+
+message="$(git tag -l --format='%(subject)' -n1 $tag)"
+git tag -fa -m "$message" $tag
+
 git push -f --all "git@github.com:v-ahadke/public_repo.git"
 #git push -f --tags "git@github.com:v-ahadke/public_repo.git"
 git push -f "git@github.com:v-ahadke/public_repo.git" $tag
